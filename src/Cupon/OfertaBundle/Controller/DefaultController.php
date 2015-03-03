1c648079019a5c31251c052e90f7ad7fc291debc
Ruben L B <rubiolb@hotmail.com>
2015-03-03 15:53:30 -0400

    Pagina 173. Antes de comenzar"Seleccionando ciudad activa". Concluida la portada con plantillas Twig 3 niveles e incluidas 3 hojas de estilo.

--- src/Cupon/OfertaBundle/Controller/DefaultController.php
+++ src/Cupon/OfertaBundle/Controller/DefaultController.php
@@ -4,11 +4,24 @@ namespace Cupon\OfertaBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class DefaultController extends Controller
 {
     public function ayudaAction()
     {
         return $this->render('OfertaBundle:Default:ayuda.html.twig'); 
+    }
+    public function portadaAction($ciudad = null)
+    {
+        $em = $this->getDoctrine()->getManager();
+        
+        $oferta = $em->getRepository('OfertaBundle:Oferta')->findOfertaDelDia($ciudad);
+        
+        if (!$oferta) {
+            throw $this->createNotFoundException(':O !..No se ha encontrado la oferta del día en la ciudad seleccionada');
+        }
+
+        return $this->render('OfertaBundle:Default:portada.html.twig',array('oferta' => $oferta));
     }
 }
